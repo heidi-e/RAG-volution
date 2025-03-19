401081752eb67225b3d2ac559eba29c0ef9102d3
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-03-19 09:32:11 -0400

    Added some initial information to the otherdb file (ingest)

--- src/other_database/otherdb_ingest.py
+++ src/other_database/otherdb_ingest.py
@@ -1,1 +1,12 @@
 import ollama
+import json
+from sentence_transformers import SentenceTransformer
+
+#Decide on the model to use:
+embedding_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+
+#Create an embedding for a query:
+def encode_text(info):
+
+    return embedding_model.encode(info).tolist()
